b5ba338a19ed823b44c67526e73930bf1a18e8d2
Wakhungu N Rebecca <rebeccanakhanu@gmail.com>
2023-04-19 20:17:39 +0300

    completed assignments

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,11 +1,24 @@
+// import "@testing-library/jest-dom";
+// import { render, screen } from "@testing-library/react";
+// import React from "react";
+
+// import Header from "../components/Header";
+
+// test("displays the text 'hello from the Header!'", () => {
+//   render(<Header />);
+
+//   expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
+// });
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 
 import Header from "../components/Header";
 
-test("displays the text 'hello from the Header!'", () => {
-  render(<Header />);
+test("displays the text 'please pass this test'", () => {
+render(<Header />);
 
-  expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
+expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
+
+
